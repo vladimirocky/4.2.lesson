787010a4fae8c3aa6a8b4d94fcbfdd11c078a66b
Ivan Haliurov <galyurov@gmail.com>
2022-05-11 23:46:27 +0300

    add examples

---
+++ pre_examples/webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  module: {
+    rules: [
+      //scss-sass loader
+      {
+        test: /\.s[ac]ss$/i, // проверка расширения файлов (sass,scss)
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      // css loader
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
